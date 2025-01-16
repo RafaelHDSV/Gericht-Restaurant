32313d0c479b13c58fa87c84c1d4daed61ad891e
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-16 19:44:50 -0300

    feat: implement responsive Navbar with toggle menu for small screens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import { ForkKnife, List } from '@phosphor-icons/react'
 import images from '../../constants/images'
 import styles from './Navbar.module.scss'
+import { useState } from 'react'
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
@@ -33,28 +36,32 @@ export default function Navbar() {
         <a href='/'>Book Table</a>
       </div>
 
-      <button className={styles.navbarSmallScreens} onClick={() => console.log('clicked')}>
-        <List size={26} />
-        <ForkKnife size={26} />
+      <div className={styles.navbarSmallScreens} onClick={() => console.log('clicked')}>
+        <List onClick={() => setToggleMenu(prev => !prev)} size={26} />
 
-        <ul className={styles.linksSmallScreens}>
-          <li>
-            <a href='#home'>Home</a>
-          </li>
-          <li>
-            <a href='#about'>About</a>
-          </li>
-          <li>
-            <a href='#menu'>Menu</a>
-          </li>
-          <li>
-            <a href='#awards'>Awards</a>
-          </li>
-          <li>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
-      </button>
+        {toggleMenu && (
+          <div className={styles.navbarSmallScreensContent}>
+            <ForkKnife onClick={() => setToggleMenu(prev => !prev)} size={26} className={styles.navbarSmallScreensClose} />
+            <ul className={styles.linksSmallScreens}>
+              <li>
+                <a href='#home'>Home</a>
+              </li>
+              <li>
+                <a href='#about'>About</a>
+              </li>
+              <li>
+                <a href='#menu'>Menu</a>
+              </li>
+              <li>
+                <a href='#awards'>Awards</a>
+              </li>
+              <li>
+                <a href='#contact'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   )
 }
