b6ce509f96a5774ec890fc9bd512f7e08ce56a0e
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-16 19:44:54 -0300

    chore: comment out unused components in App.tsx for cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import Navbar from './components/Navbar/Navbar'
-import Header from './container/Header/Header'
-import AboutUs from './container/AboutUs/AboutUs'
-import SpecialMenu from './container/Menu/SpecialMenu'
-import Chef from './container/Chef/Chef'
-import Intro from './container/Intro/Intro'
-import Laurels from './container/Laurels/Laurels'
-import Gallery from './container/Gallery/Gallery'
-import FindUs from './container/Findus/FindUs'
-import Footer from './container/Footer/Footer'
 import './App.module.scss'
 
 const App = () => (
   <div>
     <Navbar />
-    <Header />
+    {/* <Header />
     <AboutUs />
     <SpecialMenu />
     <Chef />
@@ -21,7 +12,7 @@ const App = () => (
     <Laurels />
     <Gallery />
     <FindUs />
-    <Footer />
+    <Footer /> */}
   </div>
 )
 
