207c30cd3b769da09d8f5d62c3bb3cd68bbd4ee7
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-16 22:14:17 -0300

    feat: update header component

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { SubHeading } from '../../components'
+import images from '../../constants/images'
+import baseStyles from '../../App.module.scss'
+import styles from './Header.module.scss'
 
-import './Header.css';
+export default function Header() {
+  return (
+    <div className={`${baseStyles.app__wrapper} ${baseStyles.section__padding} ${styles.header}`}>
+      <div className={baseStyles.app__wrapper_info}>
+        <SubHeading>Chase the new flavor</SubHeading>
+        <h1 className={styles.h1}>The key to fine dining</h1>
+        <p className={baseStyles.p__opensans} style={{ margin: '2rem 0' }}>
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facillisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
+        </p>
+        <button className={baseStyles.custom__button}>Explore Menu</button>
+      </div>
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
-
-export default Header;
+      <div className={baseStyles.app__wrapper_img}>
+        <img src={images.welcome} alt='welcome' />
+      </div>
+    </div>
+  )
+}
