d19a29352c92e714fa5b577b3fb5d78f7351944f
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-16 22:15:16 -0300

    refactor: rename font class and clean up unused components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar/Navbar'
+import Header from './container/Header/Header'
 import './App.module.scss'
 
 const App = () => (
   <div>
     <Navbar />
-    {/* <Header />
-    <AboutUs />
+    <Header />
+    {/* <AboutUs />
     <SpecialMenu />
     <Chef />
     <Intro />
