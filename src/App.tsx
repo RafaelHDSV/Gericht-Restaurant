303486230000138092a81fdd1d6441fcd6fd2f14
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-17 22:35:27 -0300

    feat: integrate SpecialMenu component into App and update image imports in AboutUs and SubHeading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Navbar from './components/Navbar/Navbar'
 import Header from './container/Header/Header'
 import AboutUs from './container/AboutUs/AboutUs'
+import SpecialMenu from './container/Menu/SpecialMenu'
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <AboutUs />
-    {/* <SpecialMenu />
-    <Chef />
+    <SpecialMenu />
+    {/*<Chef />
     <Intro />
     <Laurels />
     <Gallery />
