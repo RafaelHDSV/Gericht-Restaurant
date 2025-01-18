d21eddd4c9284865086ab467be1851e3628ebbc4
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-18 04:36:26 -0300

    feat: add SCSS module import to App for improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Laurels from './container/Laurels/Laurels'
 import Gallery from './container/Gallery/Gallery'
 import FindUs from './container/Findus/FindUs'
 import Footer from './container/Footer/Footer'
+import './App.module.scss'
 
 const App = () => (
   <div>
