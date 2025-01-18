c7f9edecedb7ce700ebbd27935a215c56e9a5c0d
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-17 22:53:22 -0300

    feat: add Chef component with styles and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar/Navbar'
 import Header from './container/Header/Header'
 import AboutUs from './container/AboutUs/AboutUs'
 import SpecialMenu from './container/Menu/SpecialMenu'
+import Chef from './container/Chef/Chef'
 
 const App = () => (
   <div>
@@ -9,8 +10,8 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
-    {/*<Chef />
-    <Intro />
+    <Chef />
+    {/*<Intro />
     <Laurels />
     <Gallery />
     <FindUs />
--- src/container/Chef/Chef.tsx
+++ src/container/Chef/Chef.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import images from '../../constants/images'
+import SubHeading from '../../components/SubHeading/SubHeading'
+import baseStyles from '../../App.module.scss'
+import styles from './Chef.module.scss'
 
-import './Chef.css';
+export default function Chef() {
+  return (
+    <div className={`${baseStyles.app__bg} ${baseStyles.app__wrapper} ${baseStyles.section__padding}`}>
+      <div className={`${baseStyles.app__wrapper_img} ${baseStyles.app__wrapper_img_reverse}`}>
+        <img src={images.chef} alt='chef' />
+      </div>
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+      <div className={baseStyles.app__wrapper_info}>
+        <SubHeading>Chef's Word</SubHeading>
+        <h1 className={baseStyles.headtext__cormorant}>What We Believe In</h1>
 
-export default Chef;
+        <div className={styles.content}>
+          <div className={styles.quote}>
+            <img src={images.quote} alt='quote' />
+            <p className={baseStyles.p__opensans}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <p className={baseStyles.p__opensans}>
+            Ex iure ea laudantium, autem pariatur veniam at soluta aliquid velit quasi ratione voluptate magnam eligendi distinctio est voluptatibus magni
+            laboriosam possimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fugiat illo quae quas possimus eos
+          </p>
+        </div>
+
+        <div className={styles.sign}>
+          <p>Kevin Luo</p>
+          <p className={baseStyles.p__opensans}>Chef & Founder</p>
+          <img src={images.sign} alt='sign' />
+        </div>
+      </div>
+    </div>
+  )
+}
