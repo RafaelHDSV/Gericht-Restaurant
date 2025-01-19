1bd0bf9c43f07e0740d5d35614b162e09b35cd92
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:36:19 -0300

    feat: refactor FindUs component to use SCSS module imports and simplify styling

--- src/container/Findus/FindUs.tsx
+++ src/container/Findus/FindUs.tsx
@@ -1,31 +1,25 @@
 import SubHeading from '../../components/SubHeading/SubHeading'
 import images from '../../constants/images'
-import baseStyles from '../../App.module.scss'
+import styles from './FindUs.module.scss'
 
 export default function FindUs() {
   return (
-    <div id='contact' className={`${baseStyles.app__bg} ${baseStyles.app__wrapper} ${baseStyles.section__padding} `}>
-      <div className={baseStyles.app__wrapper_info}>
+    <div id='contact' className={styles.contact}>
+      <div className={styles.info}>
         <SubHeading>Contact</SubHeading>
-        <h1 className={baseStyles.headtext__cormorant} style={{ marginBottom: '3rem' }}>
-          Find Us
-        </h1>
+        <h1>Find Us</h1>
 
-        <div className={baseStyles.app__wrapper_info}>
-          <p className={baseStyles.p__opensans}>Lane Ends Bungalow, Whatcroft Hall Lane, RUdheath, CW9 7SG</p>
-          <p className={baseStyles.p__cormorant} style={{ color: '#DCCA87', margin: '2rem 0' }}>
-            Opening Hours
-          </p>
-          <p className={baseStyles.p__opensans}>Mon - Fri: 10:00 am - 02:00 am</p>
-          <p className={baseStyles.p__opensans}>Sat - Sun: 10:00 am - 03:00 am</p>
+        <div className={styles.info}>
+          <p>Lane Ends Bungalow, Whatcroft Hall Lane, RUdheath, CW9 7SG</p>
+          <p>Opening Hours</p>
+          <p>Mon - Fri: 10:00 am - 02:00 am</p>
+          <p>Sat - Sun: 10:00 am - 03:00 am</p>
         </div>
 
-        <button className={baseStyles.custom__button} style={{ marginTop: '2rem' }}>
-          Visit Us
-        </button>
+        <button>Visit Us</button>
       </div>
 
-      <div className={baseStyles.app__wrapper_img}>
+      <div className={styles.img}>
         <img src={images.findus} alt='findus' />
       </div>
     </div>
