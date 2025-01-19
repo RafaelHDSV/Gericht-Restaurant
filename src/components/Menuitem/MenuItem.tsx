8d04d7204e2892106a22389b81504f9aed2fdec7
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:36:33 -0300

    feat: refactor MenuItem component to use SCSS module imports and simplify styling

--- src/components/Menuitem/MenuItem.tsx
+++ src/components/Menuitem/MenuItem.tsx
@@ -1,4 +1,3 @@
-import baseStyles from '../../App.module.scss'
 import styles from './MenuItem.module.scss'
 
 interface IMenuItemProps {
@@ -12,22 +11,18 @@ export default function MenuItem({ title, tags, price }: IMenuItemProps) {
     <div className={styles.menuItem}>
       <div className={styles.header}>
         <div className={styles.name}>
-          <p className={baseStyles.p__cormorant} style={{ color: '#DCCA87' }}>
-            {title}
-          </p>
+          <p>{title}</p>
         </div>
 
         <div className={styles.dash} />
 
         <div className={styles.price}>
-          <p className={baseStyles.p__cormorant}>{price}</p>
+          <p>{price}</p>
         </div>
       </div>
 
       <div className={styles.subInfo}>
-        <p className={baseStyles.p__opensans} style={{ color: '#AAA' }}>
-          {tags}
-        </p>
+        <p>{tags}</p>
       </div>
     </div>
   )
