1e807210b0f05d0f270c79326330b6dcd031d52c
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:35:35 -0300

    feat: refactor SubHeading component to use SCSS module imports and simplify styling

--- src/components/SubHeading/SubHeading.tsx
+++ src/components/SubHeading/SubHeading.tsx
@@ -1,12 +1,11 @@
 import images from '../../constants/images'
-import baseStyles from '../../App.module.scss'
 import styles from './SubHeading.module.scss'
 
 export default function SubHeading({ children }: { children: string }) {
   return (
     <div className={styles.container}>
-      <p className={baseStyles.p__cormorant}>{children}</p>
-      <img className={baseStyles.spoon__img} src={images.spoon} alt='spoon' />
+      <p>{children}</p>
+      <img src={images.spoon} alt='spoon' />
     </div>
   )
 }
