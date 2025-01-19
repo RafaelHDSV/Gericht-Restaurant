17df749b2a727b0b7c951bf30341040e0a2deecd
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:36:01 -0300

    feat: refactor Header component to use SCSS module imports and simplify styling

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,21 +1,20 @@
 import SubHeading from '../../components/SubHeading/SubHeading'
 import images from '../../constants/images'
-import baseStyles from '../../App.module.scss'
 import styles from './Header.module.scss'
 
 export default function Header() {
   return (
-    <div className={`${baseStyles.app__wrapper} ${baseStyles.section__padding} ${styles.header}`}>
-      <div className={baseStyles.app__wrapper_info}>
+    <div className={styles.header}>
+      <div className={styles.info}>
         <SubHeading>Chase the new flavor</SubHeading>
-        <h1 className={styles.h1}>The key to fine dining</h1>
-        <p className={baseStyles.p__opensans} style={{ margin: '2rem 0' }}>
+        <h1>The key to fine dining</h1>
+        <p>
           Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facillisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
         </p>
-        <button className={baseStyles.custom__button}>Explore Menu</button>
+        <button>Explore Menu</button>
       </div>
 
-      <div className={baseStyles.app__wrapper_img}>
+      <div className={styles.img}>
         <img src={images.welcome} alt='welcome' />
       </div>
     </div>
