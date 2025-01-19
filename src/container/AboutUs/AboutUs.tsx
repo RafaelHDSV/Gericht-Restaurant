2233ae99e463328f6922753e39e505d55e2212c1
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:36:28 -0300

    feat: refactor AboutUs component to use SCSS module imports and simplify styling

--- src/container/AboutUs/AboutUs.tsx
+++ src/container/AboutUs/AboutUs.tsx
@@ -1,37 +1,36 @@
 import images from '../../constants/images'
-import baseStyles from '../../App.module.scss'
 import styles from './AboutUs.module.scss'
 
 export default function AboutUs() {
   return (
-    <div id='about' className={`${styles.aboutContainer} ${baseStyles.section__padding} ${baseStyles.flex__center} ${baseStyles.app__bg}`}>
-      <div className={`${styles.aboutBackground} ${baseStyles.flex__center}`}>
+    <div id='about' className={styles.aboutContainer}>
+      <div className={styles.aboutBackground}>
         <img src={images.G} alt='g' />
       </div>
 
-      <div className={`${styles.aboutContent} ${baseStyles.flex__center}`}>
+      <div className={styles.aboutContent}>
         <div className={styles.aboutLeftContainer}>
-          <h1 className={baseStyles.headtext__cormorant}>About Us</h1>
-          <img className={baseStyles.spoon__img} src={images.spoon} alt='spoon' />
-          <p className={baseStyles.p__opensans}>
+          <h1>About Us</h1>
+          <img src={images.spoon} alt='spoon' />
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae impedit harum nam quas nemo, similique exercitationem, mollitia, vel doloribus vero
             sunt eos error ipsum nostrum. Corporis officia architecto harum! Qui.
           </p>
-          <button className={baseStyles.custom__button}>Know More</button>
+          <button>Know More</button>
         </div>
 
-        <div className={`${styles.knife} ${baseStyles.flex__center}`}>
+        <div className={styles.knife}>
           <img src={images.knife} alt='knife' />
         </div>
 
         <div className={styles.aboutRightContainer}>
-          <h1 className={baseStyles.headtext__cormorant}>Our History</h1>
-          <img className={baseStyles.spoon__img} src={images.spoon} alt='spoon ' />
-          <p className={baseStyles.p__opensans}>
+          <h1>Our History</h1>
+          <img src={images.spoon} alt='spoon ' />
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae impedit harum nam quas nemo, similique exercitationem, mollitia, vel doloribus vero
             sunt eos error ipsum nostrum. Corporis officia architecto harum! Qui.
           </p>
-          <button className={baseStyles.custom__button}>Know More</button>
+          <button>Know More</button>
         </div>
       </div>
     </div>
