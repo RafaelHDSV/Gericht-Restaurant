ae1d73f863bd7ceb7960184681f2637f4ddbbbb0
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-01-19 00:36:24 -0300

    feat: refactor Chef component to use SCSS module imports and simplify styling

--- src/container/Chef/Chef.tsx
+++ src/container/Chef/Chef.tsx
@@ -1,25 +1,24 @@
 import images from '../../constants/images'
 import SubHeading from '../../components/SubHeading/SubHeading'
-import baseStyles from '../../App.module.scss'
 import styles from './Chef.module.scss'
 
 export default function Chef() {
   return (
-    <div className={`${baseStyles.app__bg} ${baseStyles.app__wrapper} ${baseStyles.section__padding}`}>
-      <div className={`${baseStyles.app__wrapper_img} ${baseStyles.app__wrapper_img_reverse}`}>
+    <div className={styles.chef}>
+      <div className={styles.chefImage}>
         <img src={images.chef} alt='chef' />
       </div>
 
-      <div className={baseStyles.app__wrapper_info}>
+      <div className={styles.info}>
         <SubHeading>Chef's Word</SubHeading>
-        <h1 className={baseStyles.headtext__cormorant}>What We Believe In</h1>
+        <h1>What We Believe In</h1>
 
         <div className={styles.content}>
           <div className={styles.quote}>
             <img src={images.quote} alt='quote' />
-            <p className={baseStyles.p__opensans}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
-          <p className={baseStyles.p__opensans}>
+          <p>
             Ex iure ea laudantium, autem pariatur veniam at soluta aliquid velit quasi ratione voluptate magnam eligendi distinctio est voluptatibus magni
             laboriosam possimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fugiat illo quae quas possimus eos
           </p>
@@ -27,7 +26,7 @@ export default function Chef() {
 
         <div className={styles.sign}>
           <p>Kevin Luo</p>
-          <p className={baseStyles.p__opensans}>Chef & Founder</p>
+          <p>Chef & Founder</p>
           <img src={images.sign} alt='sign' />
         </div>
       </div>
