9d1f0adeb4e95c816dc13b5177f76ac3d1a635b4
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-23 22:17:23 -0300

    feat: add vieira analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { VieiraAnalytics } from '@vieira/analytics/react'
 import App from './App.tsx'
 import './main.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <VieiraAnalytics projectKey="gericht-restaurant" />
   </StrictMode>
 )
